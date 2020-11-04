74c9e42fb72e52effb71adf2aea4b8fa6e54a111
Nils Martel <nilsmartel@yahoo.de>
2020-11-04 19:15:32 +0100

    Fix sed script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# cp zshrc $HOME/.zshrc
-sed "s|{{WHICH_ZSH}}|$(which zsh)|g" \
-    "s/{{TERM}}/${TERM}/g" \
-    tmux.conf # > $HOME/.tmux.conf
+cp zshrc $HOME/.zshrc
+sed -e "s|{{WHICH_ZSH}}|$(which zsh)|g" \
+    -e "s/{{TERM}}/${TERM}/g" \
+    tmux.conf > $HOME/.tmux.conf
 
 
