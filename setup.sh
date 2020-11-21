cdf2194db4e2a6ffe9d580c7017ad067262f41d0
Nils Martel <nils.martel@graphmasters.net>
2020-11-21 20:11:10 +0100

    Use proper templating for configs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-cp zshrc $HOME/.zshrc
-sed -e "s|{{WHICH_ZSH}}|$(which zsh)|g" \
-    -e "s/{{TERM}}/${TERM}/g" \
-    tmux.conf > $HOME/.tmux.conf
+echo "finding cargo"
+if ! command -v cargo; then
+    echo "installing cargo"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
+echo "finding shell-string"
+if ! command -v string; then
+    echo "installing shell-string"
+    cargo install shell-string
+fi
 
+string template <./zshrc-template >"$HOME/.zshrc"
+string template <./tmux.conf-template >"$HOME/.tmux.conf"
