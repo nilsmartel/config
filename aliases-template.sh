b1f6a9eb1006a8dfac1ec6ff7e4ee057f8662453
= <=>
2022-04-20 14:44:17 +0200

    Update config

--- aliases-template.sh
+++ aliases-template.sh
@@ -15,44 +15,18 @@ alias push="git push"
 alias st="git status"
 alias stage="git stage"
 alias merge="git merge"
-alias gitignore="vim .gitignore && git stage .gitignore && git commit -m 'Update gitignore'"
 alias out=checkout
 
-alias dc="docker-compose"
 alias k="kubectl"
 
 # Prefer nvim
 alias vim=nvim
-# Handy shortcut to launch my favourite vim gui instead
-alias vi=neovide
-
-alias c=cargo
-alias example='cargo run --example'
-
-# Syntax Highlighting
-alias yaml="bat -pl yaml"
-alias json="bat -pl json"
-
-alias list='exa -1'
-alias tree='exa -T'
-alias overview='exa -R -L 2'
 
 # Opening Files with goland IDE
 alias goland='open -a GoLand'
-alias rust=evcxr
 alias cat='bat -p'
-
-alias python="{{which python3}}"
 
 alias github='open -a Github\ Desktop'
 
 alias npm-registry-github="npm set registry https://npm.pkg.github.com"
 alias npm-registry-npm="npm set registry https://registry.npmjs.org/"
-
-alias yeagi='rlwrap yaegi'
-# I always have a type in the word "yaegi"
-alias zaegi='rlwrap yaegi'
-alias yeagi='rlwrap yaegi'
-
-alias ubuntu='docker run -ti ubuntu bash'
-alias alpine='docker run -ti alpine sh'
--- setup.sh
+++ setup.sh
@@ -5,6 +5,17 @@ git config --global init.defaultBranch main
 git config --global user.name = "Nils Martel"
 git config --global user.email = "nilsmartel@yahoo.de"
 
+# Install neovim (latest version)
+brew install --HEAD neovim
+# installing vimplug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+brew install --HEAD tmux
+brew install --HEAD zsh
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "finding cargo"
 if ! command -v cargo; then
@@ -19,6 +30,7 @@ if ! command -v string; then
     cargo install shell-string
 fi
 
+
 echo "finding git-delta"
 if ! command -v delta; then
     echo "installing git-delta"
@@ -30,3 +42,18 @@ string template <./tmux.conf-template >"$HOME/.tmux.conf"
 string template <./aliases-template.sh >"$HOME/aliases.sh"
 string template <./alacritty-template.yml >"$HOME/.alacritty.yml"
 cp gitconfig $HOME/.gitconfig
+
+echo
+echo "less essential but still useful"
+
+echo "finding ripgrep"
+if ! command -v rg; then
+    echo "installing ripgrep"
+    cargo install ripgrep
+fi
+
+echo "finding bat"
+if ! command -v bat; then
+    echo "installing bat"
+    cargo install bat
+fi
